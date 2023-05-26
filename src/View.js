3e2a6e69ff1e63167b55fbb231cba1496882dd2f
MarineMi <m.g.vasyukova@gmail.com>
2023-05-26 15:22:00 +0300

    registration done

--- src/View.js
+++ src/View.js
@@ -10,8 +10,15 @@ class View {
 
     // Тут всё рисуем.
     console.clear();
+    console.log(
+      `Наш герой: ${this.game.hero.name}\n${this.game.hero.lifes}\nТекущий счет: ${this.game.hero.scores}\n\n`
+    );
     console.log(this.game.track.join(''));
-    console.log('\n\n');
+    console.log('\n');
+    if (this.game.hero.lifes === 0) {
+      console.log(`Твой общий результат: ${this.game.hero.bigscore}`);
+    }
+
     console.log(`Created by "${yourTeamName}" with love`);
   }
 }
